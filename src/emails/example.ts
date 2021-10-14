9fd0e9492d2cb542115d62aba3137f9362a64921
imprevo <zhenek42@gmail.com>
2021-10-14 18:21:37 +0700

    add start script

---
+++ src/emails/example.ts
@@ -0,0 +1,3 @@
+export const exampleEmail1 = () => 'Example 1';
+
+export const exampleEmail2 = () => `<div>Example 2</div>`;
---
+++ src/server/index.ts
@@ -0,0 +1,4 @@
+import { exampleEmail1, exampleEmail2 } from '../emails/example';
+
+console.log(exampleEmail1());
+console.log(exampleEmail2());
