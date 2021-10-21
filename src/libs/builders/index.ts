03433d55f02710f3cbd7e1c9a3f4222bcedb34d7
imprevo <zhenek42@gmail.com>
2021-10-21 10:34:13 +0700

    include plugins in builders

--- src/libs/builders/index.ts
+++ src/libs/builders/index.ts
@@ -1,18 +1,23 @@
 import { renderToStaticMarkup } from 'react-dom/server';
 import { TemplateBuilder } from '../core';
+import { inlineCSS } from '../plugins';
+
+const plugins = [inlineCSS];
 
 export const createStringTemplate = <Params = void>(
   title: string,
   template: (params: Params) => string
 ) => {
-  return new TemplateBuilder<Params>(title, template);
+  return new TemplateBuilder<Params>(title, template, plugins);
 };
 
 export const createReactTemplate = <Params = void>(
   title: string,
   template: (params: Params) => JSX.Element
 ) => {
-  return new TemplateBuilder<Params>(title, (params) =>
-    renderToStaticMarkup(template(params))
+  return new TemplateBuilder<Params>(
+    title,
+    (params) => renderToStaticMarkup(template(params)),
+    plugins
   );
 };
